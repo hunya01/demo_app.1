88604d542166358ec6c1f893a37ff3795e167cd1
EC2 Default User <ec2-user@ip-172-31-18-55.us-east-2.compute.internal>
2018-06-16 01:37:03 +0000

    Make a basic User model

--- demo_app/config/routes.rb
+++ demo_app/config/routes.rb
@@ -1,4 +1,6 @@
-Rails.application.routes.draw do
-  resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+SampleApp::Application.routes.draw do
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
 end
